d3b14ee4510938cfd39939c8cabee552f7cc5f07
babydu <31267007+babydu@users.noreply.github.com>
2020-03-06 22:22:22 +0800

    添加英文版

--- js/prince.js
+++ js/prince.js
@@ -1,6 +1,7 @@
 //返回顶部
 // 当网页向下滑动 20px 出现"返回顶部" 按钮
-window.onscroll = function() {scrollFunction()};
+//功能调整，下滑后再显示返回顶部按钮失效。
+//window.onscroll = function() {scrollFunction()};
  
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -10,9 +11,10 @@ function scrollFunction() {
         
     }
 }
-function topFunction() {
+function topFunction(id) {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+	displayButtonMenu(id);
 }//返回顶部
 
 //显示章节目录
@@ -25,5 +27,15 @@ window.onload=function theUrl(){
 	document.getElementById("thePageUrl").href = theurl;
 	console.log("换url");
 }
-
+//显示隐藏
+function displayButtonMenu(id){
+                var sub_menu = document.getElementById(id);
+                var dis_v = sub_menu.style.display;
+                
+                if(dis_v == "block")
+                    sub_menu.style.display = "none";
+                else
+                    sub_menu.style.display = "block";
+                    
+            }
 
